dae6050ec75298736378dccb6f8bf01a7ebe693b
matt joseloff <msjo425@gmail.com>
2020-07-08 20:32:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,8 @@
 def position_taken?(board,index)
   if board[index] == "" || board[index] == " "
      false
+   elsif board[index] = nil
+     false
 #elsif board[index] == "X" || board[index] == "O"
 #  true
 end
